d9e7f5a2d0d2a109812489f6a2855ef793bf7f07
AnnYoS <simon.yoann54@gmail.com>
2021-02-10 22:26:11 +0100

    exit salon vocal

--- runner/vocal/vocal.go
+++ runner/vocal/vocal.go
@@ -5,18 +5,28 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
+var voiceConnection *discordgo.VoiceConnection
+
 func JoinVocal(session *discordgo.Session, message *discordgo.Message, param string, options []model.Option) {
 	g, _ := session.State.Guild(message.GuildID)
 	u, _ := session.GuildMember(message.GuildID, message.Author.ID)
 
+	var connection *discordgo.VoiceConnection
 	for _, state := range g.VoiceStates {
 		if state.UserID == u.User.ID {
 			channel, _ := session.State.Channel(state.ChannelID)
-			_, _ = session.ChannelVoiceJoin(message.GuildID, channel.ID, true, false)
+			connection, _ = session.ChannelVoiceJoin(message.GuildID, channel.ID, true, false)
 		}
 	}
+	voiceConnection = connection
 }
 
 func ExitVocal(session *discordgo.Session, message *discordgo.Message, param string, options []model.Option) {
-
+	if voiceConnection != nil {
+		voiceConnection.Close()
+		err := voiceConnection.Disconnect()
+		if err != nil {
+			_, _ = session.ChannelMessageSend(message.ChannelID, "Je ne peux pas me déconnecter si je ne suis même pas en vocal")
+		}
+	}
 }
